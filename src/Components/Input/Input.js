afaaa0c8a5d8f1d3774851877e7b8a7d8787005e
rpritr <robi@foreach-labs.si>
2026-01-13 11:38:08 +0100

    add button and form

---
+++ src/Components/Input/Input.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import styles from "./_input.module.scss";
+
+const Input = ({title, id, name}) => {
+    return (
+        <input id={id} name={name} className={styles.formControl} placeholder={title} />
+    )
+}
+export default Input;
